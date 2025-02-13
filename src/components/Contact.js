bf7ec16a463135d48c47357b1d7b46f9ac9f76de
anam <javedanam70@gmail.com>
2025-02-14 00:28:57 +0530

    Added-Routing

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () =>{
+    return(
+        <div>
+            <h1>Hola!</h1>
+            <h1>Welcome to the Contact Page!</h1>
+        </div>
+    )
+}
+
+export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const[btnName,setBtnName] = useState("Login");
@@ -10,9 +11,9 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                     <li>Cart</li>
                     <button className="login-btn" onClick={()=>{
                         btnName === "Login"?setBtnName("Logout"):setBtnName("Login");
